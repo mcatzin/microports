d96df6ccf26f27de6697cd52adb097f4138f4c96
Mario Catzin <catzin21@gmail.com>
2020-07-01 13:40:55 -0800

    first commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+// const person = require('./mymodule');
+
+
+// console.log(person.name);
+//es2015 Module
+// import { person } from './mymodule'
+// import * as mod from './mymodule'
+import greeting from './mymodule'
